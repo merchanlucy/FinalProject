34c9ec7fca4e5d2a4a8e1b646464ef25d918d3d1
Lucy <merchanlucy39@gmail.com>
2025-12-14 17:58:31 -0500

    test displayScores method from Course class in Main class

--- src/main/java/org/lucy/Main.java
+++ src/main/java/org/lucy/Main.java
@@ -1,17 +1,24 @@
 package org.lucy;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        Address address = new Address(1, "Rue", "Montreal", Address.Province.QC, "X1X2X3");
+        Department department = new Department("Computer Science");
+        Student student1 = new Student("Luciana Merchan", Student.Gender.FEMALE, address, department);
+        Student student2 = new Student("Juan Garcia", Student.Gender.MALE, address, department);
+        Student student3 = new Student("Valentina Vera", Student.Gender.FEMALE, address, department);
+        Course course = new Course("Intro to Programming", 2.66, department);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        course.registerStudent(student1);
+        course.registerStudent(student3);
+        course.registerStudent(student2);
+
+        course.addAssignment("Lab01", 20);
+        course.addAssignment("Lab02", 20);
+        course.addAssignment("Exam01", 30);
+        course.addAssignment("Exam02", 30);
+        course.generateScores();
+
+        course.displayScores();
     }
 }
